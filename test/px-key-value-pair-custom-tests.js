6eab4ff945a74785cd7fd1865ecd46be0ac7f5fa
Martin Wragg <mdwragg@gmail.com>
2017-11-08 14:59:47 -0800

    Makes tests great

--- test/px-key-value-pair-custom-tests.js
+++ test/px-key-value-pair-custom-tests.js
@@ -1,4 +1,4 @@
-suite('Custom Automation Tests for px-key-value-pair', function() {
+suite('Custom Automation Tests for px-key-value-pair', function(done) {
   let kvp;
   setup((done)=> {
     kvp = fixture('pxKeyValuePair');
@@ -10,36 +10,14 @@ suite('Custom Automation Tests for px-key-value-pair', function() {
   test('key-value pair element is created', function() {
     assert.isTrue(kvp !== undefined);
   });
-  test('key-value pair key gets the correct classes', function(done) {
-    let key = Polymer.dom(kvp.root).querySelector('div:first-of-type');
-    async.until(
-      ()=> {
-        return key.classList.contains('label');
-      },
-      (callback)=> {
-        setTimeout(callback, 1000);
-      },
-      ()=> {
-        assert.isTrue(key.classList.contains('label'));
-        done();
-      }
-    );
+  test('key-value pair key gets the correct classes', function() {
+    let key = Polymer.dom(kvp.root).querySelector('.label');
+    assert.isTrue(key.classList.contains('label'));
   });
-  test('key-value pair value gets the correct classes', function(done) {
-    let val = Polymer.dom(kvp.root).querySelector('div:last-of-type');
-    async.until(
-      ()=> {
-        return val.classList.contains('beta') && val.classList.contains('kvp-value--beta');
-      },
-      (callback)=> {
-        setTimeout(callback, 1000);
-      },
-      ()=> {
-        assert.isTrue(val.classList.contains('beta'));
-        assert.isTrue(val.classList.contains('kvp-value--beta'));
-        done();
-      }
-    );
+  test('key-value pair value gets the correct classes', function() {
+    let val = Polymer.dom(kvp.root).querySelector('.beta');
+    assert.isTrue(val.classList.contains('beta'));
+    assert.isTrue(val.classList.contains('kvp-value--beta'));
   });
   test('key-value pair uom text gets the correct classes', function() {
     let uom = Polymer.dom(kvp.root).querySelector('span');
