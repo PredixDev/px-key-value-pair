389e9e952ab6afd3c799b8849fe8957aecec31ed
Randy Askin <randall.askin@ge.com>
2017-11-08 10:22:03 -0800

    fix tests

--- test/px-key-value-pair-custom-tests.js
+++ test/px-key-value-pair-custom-tests.js
@@ -10,36 +10,14 @@ suite('Custom Automation Tests for px-key-value-pair', function() {
   test('key-value pair element is created', function() {
     assert.isTrue(kvp !== undefined);
   });
-  test('key-value pair key gets the correct classes', function(done) {
+  test('key-value pair key gets the correct classes', function() {
     let key = Polymer.dom(kvp.root).querySelector('div:first-of-type');
-    async.until(
-      ()=> {
-        return key.classList.contains('label');
-      },
-      (callback)=> {
-        setTimeout(callback, 1000);
-      },
-      ()=> {
-        assert.isTrue(key.classList.contains('label'));
-        done();
-      }
-    );
+    assert.isTrue(key.classList.contains('label'));
   });
-  test('key-value pair value gets the correct classes', function(done) {
+  test('key-value pair value text gets the correct classes', function() {
     let val = Polymer.dom(kvp.root).querySelector('div:last-of-type');
-    async.until(
-      ()=> {
-        return val.classList.contains('beta') && val.classList.contains('kvp-value--beta');
-      },
-      (callback)=> {
-        setTimeout(callback, 1000);
-      },
-      ()=> {
-        assert.isTrue(val.classList.contains('beta'));
-        assert.isTrue(val.classList.contains('kvp-value--beta'));
-        done();
-      }
-    );
+    assert.isTrue(val.classList.contains('beta'));
+    assert.isTrue(val.classList.contains('kvp-value--beta'));
   });
   test('key-value pair uom text gets the correct classes', function() {
     let uom = Polymer.dom(kvp.root).querySelector('span');
